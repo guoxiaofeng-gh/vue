fd6eeea0e1d153d22b7aac9108eefee12ba3899b
gxf <!gxf0324>
2021-05-25 17:07:44 +0800

    element 尝试

--- vue-admin/src/components/HelloWorld.vue
+++ vue-admin/src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
-    </el-switch>
-  </div>
+  <el-button type="text" @click="open">点击打开 Message Box</el-button>
 </template>
-
 
 <script>
   export default {
-    data() {
-      return {
-        value: true,
+    methods: {
+      open() {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
       }
-    },
+    }
   }
 </script>
